affee13d63555f3f25f7742e1222a188e793feb5
KolodnikovVyacheslav <slava@kolodnikov.com>
2025-03-31 23:27:33 +0300

    Доработал юнит тесты

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -1,43 +1,55 @@
 import Collections.Container;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UnitTest {
-    @Test
-    void addAndGet() {
-        Container container = new Container();
+    private final Container container = new Container();
+
+    @BeforeEach
+    void start() {
         container.add(1);
         container.add(2);
+        container.add(3);
+        container.add(4);
+    }
+
+    @Test
+    void addAndGet() {
         assertEquals(1, container.get(0));
         assertEquals(2, container.get(1));
     }
 
     @Test
     void remove() {
-        Container container = new Container();
-        container.add(1);
-        container.add(2);
-        container.add(3);
         container.remove(1);
         assertEquals(1, container.get(0));
         assertEquals(3, container.get(1));
-        assertEquals(2, container.size());
+        assertEquals(4, container.get(2));
+        assertEquals(3, container.size());
     }
 
     @Test
     void size() {
-        Container container = new Container();
-        assertEquals(0, container.size());
+        assertEquals(4, container.size());
         container.add(1);
-        assertEquals(1, container.size());
+        assertEquals(5, container.size());
+    }
+
+    @Test
+    void resize() {
+        for (int i = 0; i < 8; i++)
+            container.add(i);
+        assertEquals(12, container.size());
+        assertEquals(6, container.get(10));
+        assertEquals(7, container.get(11));
     }
 
     @Test
     void indexOutOfBoundsException() {
-        Container container = new Container();
-        assertThrows(IndexOutOfBoundsException.class, () -> container.get(0));
-        assertThrows(IndexOutOfBoundsException.class, () -> container.remove(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> container.get(13));
+        assertThrows(IndexOutOfBoundsException.class, () -> container.remove(99));
     }
 }
